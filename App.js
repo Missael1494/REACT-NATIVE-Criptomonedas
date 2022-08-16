a32211cabee4251776aa8cd98f486f636f9158f3
unknown <lmisa1494@gmail.com>
2022-08-16 14:21:53 -0500

    primero-Terminado

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+  ActivityIndicator
+} from 'react-native';
+import Cotizacion from './components/Cotizacion';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
+import axios from 'axios'
+
+
+
+const App = () => {
+
+  const [moneda, guardarMoneda] = useState('');
+  const [criptomoneda, guardarCriptomoneda] = useState('')
+  const [consultarAPI, guardarConsultarAPI] = useState(false)
+  const [resultado, guardarResultado] = useState({})
+  const [cargando, guardarCargando] = useState(false)
+
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async () => {
+      if(consultarAPI) {
+        //consultar la api para obtener la cotizaion
+        try {
+          const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda},ETH&tsyms=${moneda}`
+          const resultado = await axios.get(url);
+
+          guardarCargando(true)
+          //console.log(resultado.data.DISPLAY[criptomoneda][moneda])
+          setTimeout(() => {
+            guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+            guardarConsultarAPI(false)
+            guardarCargando(false)
+
+          }, 3000);
+
+          
+        } catch (error) {
+          console.log(error)
+        }
+        
+      }
+    }
+    
+    cotizarCriptomoneda();
+    
+  }, [consultarAPI])
+  
+  const componente = cargando ? <ActivityIndicator size="large" color="#5E49E2" /> : <Cotizacion resultado={resultado} />
+
+  return (
+    <>
+      <ScrollView>
+          <Header />
+
+          <Image 
+              style={styles.imagen}
+              source={require('./assets/img/cryptomonedas.png')}
+          />
+
+          <View style={styles.contenido}>
+            <Formulario 
+              moneda={moneda}
+              criptomoneda={criptomoneda}
+              guardarMoneda={guardarMoneda}
+              guardarCriptomoneda={guardarCriptomoneda}
+              guardarConsultarAPI={guardarConsultarAPI}
+            />
+          </View>
+
+          <View style={{marginTop: 40}}>
+            {componente}
+          </View>
+      </ScrollView>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%'
+  },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
+});
+
+export default App;
